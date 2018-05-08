f7447788bc4006409bdb5810173dbbf510382815
Joshua Taylor <taylor.joshua88@gmail.com>
2018-05-07 19:15:24 -0700

    Added comments to email verification code

--- StricklandPropane/StricklandPropane/Controllers/EmailController.cs
+++ StricklandPropane/StricklandPropane/Controllers/EmailController.cs
@@ -68,29 +68,39 @@ namespace StricklandPropane.Controllers
         {
             ApplicationUser user = await _userManager.FindByEmailAsync(email);
 
+            // Make sure the user exists and hasn't already confirmed their email address
             if (user is null || user.EmailConfirmed)
             {
                 return RedirectToAction("Login", "Account");
             }
 
+            // Generate the verification code and link to be sent in the e-mail
             string code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
             string verifyLink = Url.Action(nameof(Verify), "Email", new { email, code }, "https", "localhost:44306");
 
+            // Compose the e-mail message to send to the user
             var message = new SendGridMessage()
             {
                 From = new EmailAddress("taylor.joshua88@gmail.com", "Strickland Propane"),
                 Subject = "Verify Your Strickland Propane Account",
-                HtmlContent = $"<h3>Strickland Propane</h3><h4>Please verify your account by clicking the link below:</h4><a href={verifyLink}>{verifyLink}</a>",
+                HtmlContent = $"<h3>Strickland Propane</h3><h4>Please verify your account by clicking the link below:</h4><a href=\"{verifyLink}\">{verifyLink}</a>",
                 PlainTextContent = $"Please copy and paste the following link into your browser's address bar: {verifyLink}"
             };
 
+            // TODO(taylorjoshuaw): Find out if there is a way to add the recipient inside
+            //                      the initializer above rather than requiring a
+            //                      separate method call
             message.AddTo(email, $"{user.FirstName} {user.LastName}");
 
+            // Set up the SendGrid client using the SendGridAPIKey from configuration
             var client = new SendGridClient(_configuration["SendGridAPIKey"]);
             var response = await client.SendEmailAsync(message);
 
+            // Make sure the e-mail was sent successfully
             if (response.StatusCode == HttpStatusCode.Accepted)
             {
+                // Update the user's SentEmailVerification property so that we don't allow
+                // the GET request to spam inboxes unintentionally
                 user.SentEmailVerification = true;
                 await _userManager.UpdateAsync(user);
                 return View("Verify", email);
